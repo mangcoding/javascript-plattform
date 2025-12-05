52cf776868e262e27b338dd40c715345151bac6a
nugrahaindie <nugraha@indietechsolutions.com>
2025-12-05 14:25:24 +0700

    sample prisma with generator

--- week-10/clean-app/services/userService.js
+++ week-10/clean-app/services/userService.js
@@ -1,9 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
-
-// Singleton pattern to ensure Prisma Client is only instantiated once
-const prisma = new PrismaClient({
-  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-});
+const prisma = require('../prisma/client');
 
 const getAllUsers = async () => {
     const users = await prisma.user.findMany();
